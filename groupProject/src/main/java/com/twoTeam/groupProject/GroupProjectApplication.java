20ad2991fd40e240a96b518fdb4174ecfecc59d4
Fish <sm29729443@gmail.com>
2023-12-15 16:24:32 +0800

    在 UserInterceptor 實作出根據權限決定那些 API 可以被訪問

--- groupProject/src/main/java/com/twoTeam/groupProject/GroupProjectApplication.java
+++ groupProject/src/main/java/com/twoTeam/groupProject/GroupProjectApplication.java
@@ -4,13 +4,17 @@ package com.twoTeam.groupProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLOutput;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GroupProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroupProjectApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(GroupProjectApplication.class, args);
+		System.out.println("stop");
 	}
 
 }
--- groupProject/src/main/java/com/twoTeam/groupProject/constants/UserRoles.java
+++ groupProject/src/main/java/com/twoTeam/groupProject/constants/UserRoles.java
@@ -2,5 +2,12 @@ package com.twoTeam.groupProject.constants;
 
 
 public enum UserRoles {
-    ADMIN, MANAGER, NORMAL
+    ADMIN(0),
+    MANAGER(1),
+    NORMAL(2);
+    private Integer roleLevel;
+
+    UserRoles(Integer roleLevel) {
+        this.roleLevel = roleLevel;
+    }
 }
